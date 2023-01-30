1003fdcc0bc2819a217c082653ffcaa91a594337
Ian Virtue <ianvirtue101@gmail.com>
2023-01-30 16:26:40 -0500

    fixed error with rotatingText scss file

--- src/components/rotatingText/rotatingText.tsx
+++ src/components/rotatingText/rotatingText.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useState, useEffect } from "react";
 import { motion, useAnimation } from "framer-motion";
-import styles from "./RotatingText.module.scss";
+import styles from "../rotatingText/RotatingText.module.scss";
 
 export default function RotatingText() {
   const [text, setText] = useState("I'm Ian Virtue");
