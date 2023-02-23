838d8b40e45ca03fd0097f6b0628f67924a056f7
Ian Virtue <ianvirtue101@gmail.com>
2023-02-23 01:00:15 -0500

    Added vercel analytics:

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import "../styles/global.scss";
 import Navbar from "../components/navbar/navbar";
+import { AnalyticsWrapper } from "../components/analytics/analytics";
 
 export default function RootLayout({
   children,
@@ -18,6 +19,7 @@ export default function RootLayout({
         <Navbar />
         {children}
       </body>
+      <AnalyticsWrapper />
     </html>
   );
 }
