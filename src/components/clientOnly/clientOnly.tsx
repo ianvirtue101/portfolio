07a0134f2a443b3efdb24c31ac11f9dd5254c321
Ian Virtue <ianvirtue101@gmail.com>
2023-04-15 15:03:47 -0400

    removed unused code

--- src/components/clientOnly/clientOnly.tsx
+++ src/components/clientOnly/clientOnly.tsx
@@ -1,26 +1,26 @@
-"use client"; // imports the client module
+// "use client"; // imports the client module
 
-import { useState, useEffect } from "react"; // imports the `useState` and `useEffect` hooks from the React library
+// import { useState, useEffect } from "react"; // imports the `useState` and `useEffect` hooks from the React library
 
-export default function ClientOnly({
-  children, // a required prop that should be a React node
-}: {
-  children: React.ReactNode;
-}) {
-  // State / Props
-  const [hasMounted, setHasMounted] = useState(false); // defines a state variable `hasMounted` and a function to update it `setHasMounted`
+// export default function ClientOnly({
+//   children, // a required prop that should be a React node
+// }: {
+//   children: React.ReactNode;
+// }) {
+//   // State / Props
+//   const [hasMounted, setHasMounted] = useState(false); // defines a state variable `hasMounted` and a function to update it `setHasMounted`
 
-  // Hooks
-  useEffect(() => {
-    setHasMounted(true); // updates the `hasMounted` state variable to `true` when the component mounts
-  }, []);
+//   // Hooks
+//   useEffect(() => {
+//     setHasMounted(true); // updates the `hasMounted` state variable to `true` when the component mounts
+//   }, []);
 
-  // Render
-  if (!hasMounted) {
-    // if the component has not mounted yet, return `null`
-    return null;
-  } else {
-    // otherwise, return the `children` prop wrapped in a `div`
-    return <div>{children}</div>;
-  }
-}
+//   // Render
+//   if (!hasMounted) {
+//     // if the component has not mounted yet, return `null`
+//     return null;
+//   } else {
+//     // otherwise, return the `children` prop wrapped in a `div`
+//     return <div>{children}</div>;
+//   }
+// }
--- src/components/windowSize/windowSize.tsx
+++ src/components/windowSize/windowSize.tsx
@@ -1,14 +1,14 @@
-"use client";
-import { useState, useEffect } from "react";
+// "use client";
+// import { useState, useEffect } from "react";
 
-export default function WindowSize() {
-  const [width, setWidth] = useState(window.innerWidth);
+// export default function WindowSize() {
+//   const [width, setWidth] = useState(window.innerWidth);
 
-  useEffect(() => {
-    const handleResize = () => setWidth(window.innerWidth);
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
+//   useEffect(() => {
+//     const handleResize = () => setWidth(window.innerWidth);
+//     window.addEventListener("resize", handleResize);
+//     return () => window.removeEventListener("resize", handleResize);
+//   }, []);
 
-  return width;
-}
+//   return width;
+// }
